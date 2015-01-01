897bd26561412f6d03316e731b0c6e318ae71abf
Søren Kruse <krusenen@gmail.com>
2015-01-01 23:51:58 +0100

    Updated tests

--- BencodeNET.Tests/BStringTests.cs
+++ BencodeNET.Tests/BStringTests.cs
@@ -13,7 +13,7 @@ namespace BencodeNET.Tests
         [ExpectedException(typeof(ArgumentNullException))]
         public void ConstructorWithNullValue()
         {
-            var bstring = new BString(null);
+            var bstring = new BString((string)null);
         }
 
         [TestMethod]
@@ -40,14 +40,31 @@ namespace BencodeNET.Tests
         }
 
         [TestMethod]
-        public void HashCodeEqualsStringHashCode()
+        public void HashCodesAreEqual()
+        {
+            var bstring1 = new BString("Test String");
+            var bstring2 = new BString("Test String");
+
+            var expected = bstring1.GetHashCode();
+            var actual = bstring2.GetHashCode();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void HashCodesAreNotEqual()
         {
             var bstring = new BString("Test String");
 
-            var expected = "Test String".GetHashCode();
-            var actual = bstring.GetHashCode();
-
-            Assert.AreEqual(expected, actual);
+            Assert.AreNotEqual(bstring.GetHashCode(), new BString("Test Strin").GetHashCode());
+            Assert.AreNotEqual(bstring.GetHashCode(), new BString("Test Strin ").GetHashCode());
+            Assert.AreNotEqual(bstring.GetHashCode(), new BString("Test String ").GetHashCode());
+            Assert.AreNotEqual(bstring.GetHashCode(), new BString("Test String2").GetHashCode());
+            Assert.AreNotEqual(bstring.GetHashCode(), new BString("Test StrinG").GetHashCode());
+            Assert.AreNotEqual(bstring.GetHashCode(), new BString("test string").GetHashCode());
+            Assert.AreNotEqual(bstring.GetHashCode(), new BString("TestString").GetHashCode());
+            Assert.AreNotEqual(bstring.GetHashCode(), new BString("teststring").GetHashCode());
+            Assert.AreNotEqual(bstring.GetHashCode(), new BString("TøstStrøng").GetHashCode());
         }
 
         [TestMethod]
@@ -68,7 +85,11 @@ namespace BencodeNET.Tests
         public void Encode_UTF8()
         {
             var bstring = new BString("æøåéöñ");
-            Assert.AreEqual("6:æøåéöñ", bstring.Encode());
+
+            var expected = string.Format("{0}:æøåéöñ", Encoding.UTF8.GetByteCount("æøåéöñ"));
+            var actual = bstring.Encode(Encoding.UTF8);
+
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
@@ -82,70 +103,70 @@ namespace BencodeNET.Tests
         public void Decode_Simple()
         {
             var bstring = BString.Decode("4:spam");
-            Assert.AreEqual("spam", bstring.Value);
+            Assert.AreEqual("spam", bstring.ToString());
         }
 
         [TestMethod]
         public void Decode_UTF8()
         {
-            var bstring = BString.Decode("6:æøåéöñ", Encoding.UTF8);
-            Assert.AreEqual("æøåéöñ", bstring.Value);
+            var bstring = BString.Decode("12:æøåéöñ", Encoding.UTF8);
+            Assert.AreEqual("æøåéöñ", bstring.ToString(Encoding.UTF8));
         }
 
         [TestMethod]
         public void Decode_EmptyString()
         {
-            var bstring = BString.Decode("0:", Encoding.UTF8);
-            Assert.AreEqual("", bstring.Value);
+            var bstring = BString.Decode("0:");
+            Assert.AreEqual("", bstring.ToString());
         }
 
         [TestMethod]
         [ExpectedException(typeof(InvalidBencodeException))]
         public void Decode_Invalid_LessCharsThanSpecified()
         {
-            BString.Decode("5:spam", Encoding.UTF8);
+            BString.Decode("5:spam");
         } 
 
         [TestMethod]
         [ExpectedException(typeof(InvalidBencodeException))]
         public void Decode_Invalid_NoDelimiter()
         {
-            BString.Decode("4spam", Encoding.UTF8);
+            BString.Decode("4spam");
         } 
 
         [TestMethod]
         [ExpectedException(typeof(InvalidBencodeException))]
         public void Decode_Invalid_NonDigitFirstChar()
         {
-            BString.Decode("spam", Encoding.UTF8);
+            BString.Decode("spam");
         } 
 
         [TestMethod]
         [ExpectedException(typeof(InvalidBencodeException))]
         public void Decode_Invalid_InputMinimumLength2()
         {
-            BString.Decode("4", Encoding.UTF8);
+            BString.Decode("4");
         } 
 
         [TestMethod]
         [ExpectedException(typeof(InvalidBencodeException))]
         public void Decode_Invalid_MissingStringLength()
         {
-            BString.Decode(":spam", Encoding.UTF8);
+            BString.Decode(":spam");
         } 
 
         [TestMethod]
         [ExpectedException(typeof(UnsupportedBencodeException))]
         public void Decode_Unsupported_StringLengthAboveMaxDigits10()
         {
-            BString.Decode("12345678901:spam", Encoding.UTF8);
+            BString.Decode("12345678901:spam");
         } 
 
         [TestMethod]
         [ExpectedException(typeof(UnsupportedBencodeException))]
         public void Decode_Unsupported_StringLengthAboveInt32MaxValue()
         {
-            BString.Decode("2147483648:spam", Encoding.UTF8);
+            BString.Decode("2147483648:spam");
         } 
     }
 }
---
+++ BencodeNET.Tests/BencodeTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BencodeNET.Tests
+{
+    [TestClass]
+    [DeploymentItem("BencodeNET.Tests\\Files\\", "Files")]
+    public class BencodeTests
+    {
+        [TestMethod]
+        public void TorrentFileDecodeEncodeIsEqual()
+        {
+            const string originalFile = @"Files\ubuntu-14.10-desktop-amd64.iso.torrent";
+            const string newFile = @"Files\ubuntu-14.10-desktop-amd64.iso.test.torrent";
+
+            using (var streamRead = File.OpenRead(originalFile))
+            {
+                var bobject = Bencode.Decode(streamRead);
+                using (var streamWrite = File.OpenWrite(newFile))
+                {
+                    bobject.EncodeToStream(streamWrite);
+                }
+            }
+
+            var originalBytes = File.ReadAllBytes(originalFile);
+            var newBytes = File.ReadAllBytes(newFile);
+
+            Assert.IsTrue(originalBytes.SequenceEqual(newBytes));
+        }
+    }
+}
