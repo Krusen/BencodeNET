73646545d4880376ce686a5a869968eabbffdd2f
Søren Kruse <krusenen@gmail.com>
2015-01-02 03:53:02 +0100

    Removed unused utility extension methods

--- BencodeNET/UtilityExtensions.cs
+++ BencodeNET/UtilityExtensions.cs
@@ -9,19 +9,10 @@ namespace BencodeNET
 {
     public static class UtilityExtensions
     {
-        public static bool EndOfStream(this Stream stream)
-        {
-            return stream.Position >= stream.Length;
-        }
 
         public static void Write(this Stream stream, char c)
         {
             stream.WriteByte((byte)c);
-        }
-
-        public static void WriteAsString(this BinaryWriter stream, int integer)
-        {
-            stream.Write(integer.ToString().ToCharArray());
         }
 
         public static string AsString(this IEnumerable<char> chars)
@@ -33,11 +24,6 @@ namespace BencodeNET
             foreach (var c in chars)
                 sb.Append(c);
             return sb.ToString();
-        }
-
-        public static char ReadCharOrDefault(this BinaryReader reader)
-        {
-            return reader.ReadChars(1).FirstOrDefault();
         }
     }
 }
