710cac702fef6c6bfba832f30e719c226bea2b7d
Søren Kruse <krusenen@gmail.com>
2016-08-26 17:11:28 +0200

    Added xml doc for BObjectParser and interface

--- BencodeNET/Parsing/BObjectParser.cs
+++ BencodeNET/Parsing/BObjectParser.cs
@@ -5,30 +5,62 @@ using BencodeNET.Objects;
 
 namespace BencodeNET.Parsing
 {
+    /// <summary>
+    /// Abstract base parser for parsing bencode of specific types.
+    /// </summary>
+    /// <typeparam name="T">The type of bencode object the parser returns.</typeparam>
     public abstract class BObjectParser<T> : IBObjectParser<T> where T : IBObject
     {
+        /// <summary>
+        /// The encoding used for parsing.
+        /// </summary>
         protected abstract Encoding Encoding { get; }
 
+        /// <summary>
+        /// Parses a bencoded string into an <see cref="IBObject"/>.
+        /// </summary>
+        /// <param name="bencodedString">The bencoded string to parse.</param>
+        /// <returns>The parsed object.</returns>
         IBObject IBObjectParser.Parse(string bencodedString)
         {
             return Parse(bencodedString);
         }
 
+        /// <summary>
+        /// Parses a byte array into an <see cref="IBObject"/>.
+        /// </summary>
+        /// <param name="bytes">The bytes to parse.</param>
+        /// <returns>The parsed object.</returns>
         IBObject IBObjectParser.Parse(byte[] bytes)
         {
             return Parse(bytes);
         }
 
+        /// <summary>
+        /// Parses a stream into an <see cref="IBObject"/>.
+        /// </summary>
+        /// <param name="stream">The stream to parse.</param>
+        /// <returns>The parsed object.</returns>
         IBObject IBObjectParser.Parse(Stream stream)
         {
             return Parse(stream);
         }
 
+        /// <summary>
+        /// Parses a bencoded stream into an <see cref="IBObject"/>.
+        /// </summary>
+        /// <param name="stream">The bencoded stream to parse.</param>
+        /// <returns>The parsed object.</returns>
         IBObject IBObjectParser.Parse(BencodeStream stream)
         {
             return Parse(stream);
         }
 
+        /// <summary>
+        /// Parses a bencoded string into an <see cref="IBObject"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="bencodedString">The bencoded string to parse.</param>
+        /// <returns>The parsed object.</returns>
         public virtual T Parse(string bencodedString)
         {
             using (var stream = bencodedString.AsStream(Encoding))
@@ -37,6 +69,11 @@ namespace BencodeNET.Parsing
             }
         }
 
+        /// <summary>
+        /// Parses a byte array into an <see cref="IBObject"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="bytes">The bytes to parse.</param>
+        /// <returns>The parsed object.</returns>
         public virtual T Parse(byte[] bytes)
         {
             using (var stream = new MemoryStream(bytes))
@@ -45,11 +82,21 @@ namespace BencodeNET.Parsing
             }
         }
 
+        /// <summary>
+        /// Parses a stream into an <see cref="IBObject"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="stream">The stream to parse.</param>
+        /// <returns>The parsed object.</returns>
         public virtual T Parse(Stream stream)
         {
             return Parse(new BencodeStream(stream));
         }
 
+        /// <summary>
+        /// Parses a bencoded stream into an <see cref="IBObject"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="stream">The bencoded stream to parse.</param>
+        /// <returns>The parsed object.</returns>
         public abstract T Parse(BencodeStream stream);
     }
 }
--- BencodeNET/Parsing/IBObjectParser.cs
+++ BencodeNET/Parsing/IBObjectParser.cs
@@ -4,19 +4,71 @@ using BencodeNET.Objects;
 
 namespace BencodeNET.Parsing
 {
+    /// <summary>
+    /// A contract for parsing bencode from different sources as an <see cref="IBObject"/>.
+    /// </summary>
     public interface IBObjectParser
     {
+        /// <summary>
+        /// Parses a bencoded string into an <see cref="IBObject"/>.
+        /// </summary>
+        /// <param name="bencodedString">The bencoded string to parse.</param>
+        /// <returns>The parsed object.</returns>
         IBObject Parse(string bencodedString);
+
+        /// <summary>
+        /// Parses a byte array into an <see cref="IBObject"/>.
+        /// </summary>
+        /// <param name="bytes">The bytes to parse.</param>
+        /// <returns>The parsed object.</returns>
         IBObject Parse(byte[] bytes);
+
+        /// <summary>
+        /// Parses a stream into an <see cref="IBObject"/>.
+        /// </summary>
+        /// <param name="stream">The stream to parse.</param>
+        /// <returns>The parsed object.</returns>
         IBObject Parse(Stream stream);
+
+        /// <summary>
+        /// Parses a bencoded stream into an <see cref="IBObject"/>.
+        /// </summary>
+        /// <param name="stream">The bencoded stream to parse.</param>
+        /// <returns>The parsed object.</returns>
         IBObject Parse(BencodeStream stream);
     }
 
-    public interface IBObjectParser<T> : IBObjectParser where T : IBObject
+    /// <summary>
+    /// A contract for parsing bencode from different sources as type <typeparamref name="T"/> inheriting <see cref="IBObject"/>.
+    /// </summary>
+    public interface IBObjectParser<out T> : IBObjectParser where T : IBObject
     {
+        /// <summary>
+        /// Parses a bencoded string into an <see cref="IBObject"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="bencodedString">The bencoded string to parse.</param>
+        /// <returns>The parsed object.</returns>
         new T Parse(string bencodedString);
+
+        /// <summary>
+        /// Parses a byte array into an <see cref="IBObject"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="bytes">The bytes to parse.</param>
+        /// <returns>The parsed object.</returns>
         new T Parse(byte[] bytes);
+
+        /// <summary>
+        /// Parses a stream into an <see cref="IBObject"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="stream">The stream to parse.</param>
+        /// <returns>The parsed object.</returns>
         new T Parse(Stream stream);
+
+        /// <summary>
+        /// Parses a bencoded stream into an <see cref="IBObject"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="stream">The bencoded stream to parse.</param>
+        /// <returns>The parsed object.</returns>
         new T Parse(BencodeStream stream);
     }
 }
