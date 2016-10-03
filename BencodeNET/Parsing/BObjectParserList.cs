a84a4b1fdd028abf36170982d7d9b6afcf432018
Søren Kruse <krusenen@gmail.com>
2016-10-03 07:53:33 +0200

    Add xml doc for BObjectParserList

--- BencodeNET/Parsing/BObjectParserList.cs
+++ BencodeNET/Parsing/BObjectParserList.cs
@@ -5,25 +5,53 @@ using BencodeNET.Objects;
 
 namespace BencodeNET.Parsing
 {
+    /// <summary>
+    /// A special collection for <see cref="IBObjectParser"/> that has some extra methods
+    /// for efficiently adding and accessing parsers by the type they can parse.
+    /// </summary>
     public class BObjectParserList : IEnumerable<KeyValuePair<Type, IBObjectParser>>
     {
         private IDictionary<Type, IBObjectParser> Parsers { get; } = new Dictionary<Type, IBObjectParser>();
 
+        /// <summary>
+        /// Adds a parser for the specified type.
+        /// Existing parsers for this type will be replaced.
+        /// </summary>
+        /// <param name="type">The type this parser can parse.</param>
+        /// <param name="parser">The parser to add.</param>
         public void Add(Type type, IBObjectParser parser)
         {
             AddOrReplace(type, parser);
         }
 
+        /// <summary>
+        /// Adds a parser for the specified type.
+        /// Existing parsers for this type will be replaced.
+        /// </summary>
+        /// <param name="types">The types this parser can parse.</param>
+        /// <param name="parser">The parser to add.</param>
         public void Add(IEnumerable<Type> types, IBObjectParser parser)
         {
             AddOrReplace(types, parser);
         }
 
+        /// <summary>
+        /// Adds a specific parser.
+        /// Existing parsers for the type will be replaced.
+        /// </summary>
+        /// <typeparam name="T">The type this parser can parse.</typeparam>
+        /// <param name="parser">The parser to add.</param>
         public void Add<T>(IBObjectParser<T> parser) where T : IBObject
         {
             AddOrReplace(typeof(T), parser);
         }
 
+        /// <summary>
+        /// Adds a parser for the specified type.
+        /// Existing parsers for this type will be replaced.
+        /// </summary>
+        /// <param name="type">The type this parser can parse.</param>
+        /// <param name="parser">The parser to add.</param>
         public void AddOrReplace(Type type, IBObjectParser parser)
         {
             if (!typeof(IBObject).IsAssignableFrom(type))
@@ -34,6 +62,12 @@ namespace BencodeNET.Parsing
             Parsers.Add(type, parser);
         }
 
+        /// <summary>
+        /// Adds a parser for the specified type.
+        /// Existing parsers for this type will be replaced.
+        /// </summary>
+        /// <param name="types">The types this parser can parse.</param>
+        /// <param name="parser">The parser to add.</param>
         public void AddOrReplace(IEnumerable<Type> types, IBObjectParser parser)
         {
             foreach (var type in types)
@@ -42,33 +76,72 @@ namespace BencodeNET.Parsing
             }
         }
 
+        /// <summary>
+        /// Adds a specific parser.
+        /// Existing parsers for the type will be replaced.
+        /// </summary>
+        /// <typeparam name="T">The type this parser can parse.</typeparam>
+        /// <param name="parser">The parser to add.</param>
         public void AddOrReplace<T>(IBObjectParser<T> parser) where T : IBObject
         {
             AddOrReplace(typeof(T), parser);
         }
 
+        /// <summary>
+        /// Gets the parser, if any, for the specified type.
+        /// </summary>
+        /// <param name="type">The type to get a parser for.</param>
+        /// <returns>The parser for the specified type or null if there isn't one.</returns>
         public IBObjectParser Get(Type type)
         {
             return Parsers.GetValueOrDefault(type);
         }
 
+        /// <summary>
+        /// Gets the parser, if any, for the specified type.
+        /// </summary>
+        /// <param name="type">The type to get a parser for.</param>
+        /// <returns>The parser for the specified type or null if there isn't one.</returns>
         public IBObjectParser this[Type type]
         {
             get { return Get(type); }
             set { AddOrReplace(type, value); }
         }
 
+        /// <summary>
+        /// Gets the parser, if any, for the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to get a parser for.</typeparam>
+        /// <returns>The parser for the specified type or null if there isn't one.</returns>
         public IBObjectParser<T> Get<T>() where T : IBObject
         {
             return Get(typeof(T)) as IBObjectParser<T>;
         }
 
+        /// <summary>
+        /// Removes the parser for the specified type.
+        /// </summary>
+        /// <param name="type">The type to remove the parser for.</param>
+        /// <returns>True if successful, false otherwise.</returns>
         public bool Remove(Type type) => Parsers.Remove(type);
 
+        /// <summary>
+        /// Removes the parser for the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to remove the parser for.</typeparam>
+        /// <returns>True if successful, false otherwise.</returns>
         public bool Remove<T>() => Remove(typeof (T));
 
+        /// <summary>
+        /// Empties the collection.
+        /// </summary>
         public void Clear() => Parsers.Clear();
 
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
         public IEnumerator<KeyValuePair<Type, IBObjectParser>> GetEnumerator()
         {
             return Parsers.GetEnumerator();
